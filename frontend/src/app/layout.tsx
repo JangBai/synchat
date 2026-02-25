e0c0fbfbe87e4e6a02028afcaa55630bc2ccd356
JangBai <jbkim948@gmail.com>
2026-02-25 14:46:48 +0900

    feat: package.json src 루트 설정

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,11 +1,18 @@
+import type { Metadata } from "next";
 import localFont from "next/font/local";
-import "./globals.css";
+import "@/app/globals.css";
 
 const pretendard = localFont({
   src: "../assets/fonts/PretendardVariable.woff2",
   variable: "--font-pretendard",
   display: "swap",
 });
+
+export const metadata: Metadata = {
+  title: "Synchat",
+  description: "Synchat is a real-time state synchronization experiment.",
+  keywords: "Synchat, real-time, state synchronization, experiment",
+};
 
 export default function RootLayout({
   children,
